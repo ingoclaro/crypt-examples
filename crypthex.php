5d5bd20814dc3936b0b68f953ec0b9e5598dc728
Ingo Claro <ingo@groupon.com>
2013-03-07 15:30:32 -0600

    using hex instead of base64

---
+++ crypthex.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?php
+$config['public_key'] = 'file://./public_key.pem';
+
+$public_key = openssl_get_publickey($config['public_key']);
+
+$message = trim(fgets(STDIN));
+
+$cryptData = '';
+$ret = openssl_public_encrypt($message, $cryptData, $public_key);
+if($ret === false) {
+	die('failed to encrypt data');
+}
+
+$cryptData = bin2hex($cryptData);
+
+echo $cryptData;
---
+++ decrypthex.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+require 'openssl'
+require 'base64'
+
+def hex2bin hex
+  [hex].pack "H*"
+end
+
+private_key_file = File.read 'private_key.pem'
+private_key = OpenSSL::PKey::RSA.new private_key_file, 'password'
+
+cryptData = STDIN.read
+
+decrypted = private_key.private_decrypt hex2bin(cryptData)
+
+puts decrypted
